7c3c7ba9421ea3c1761abf2b392ada8058fc14bd
Saima <saima@Syeds-iMac.local>
2024-01-16 10:28:33 +0400

    Day13 update

--- src/test/java/myapp/tests/topics/Day13_Annotations.java
+++ src/test/java/myapp/tests/topics/Day13_Annotations.java
@@ -6,10 +6,13 @@ public class Day13_Annotations {
     /*
      @Test : is used to create test cases - same as junit
      @Before and @After annotations : there are 5 before and 5 after annotation
-     Hierarchy : suite > test > group > class > method
+     Hierarchy : suite > test > group > class > method  ... these are used to control the test flow
      @Ignore : ignore / skip a test case
-     @Test(enabled = false) : disables the test case
+     @Test(enabled = false) : disables the test case (another way of ignoring the test case) but stronger than @Ignore
+     By default tests run from 1->5 .. test 2 and 3 are disabled so by default test order will be 1,4,5.. even if you
+     put test5 on top, it will still run in the order of 1,4,5
      @Test(priority = 1) : orders / prioritize test cases - by default testNG runs in ascending order - default priority = 0
+     You can also use negative numbers like -1, -2 .. but not very commonly used
       */
     @BeforeSuite
     public void beforeSuite(){
@@ -28,8 +31,8 @@ public class Day13_Annotations {
         System.out.println("before class");
     }
     @BeforeMethod
-    public void beforeMethods(){
-        System.out.println("before methods");
+    public void beforeMethod(){
+        System.out.println("before method");
     }
     @AfterSuite
     public void afterSuite(){
