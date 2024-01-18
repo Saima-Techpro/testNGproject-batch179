4b3327dd9187a81d8457a38e4af414d78abf47de
Saima <saima@Syeds-iMac.local>
2024-01-18 10:23:09 +0400

    Driver1 updated

--- src/test/java/myapp/utilities/Driver1.java
+++ src/test/java/myapp/utilities/Driver1.java
@@ -3,6 +3,10 @@ package myapp.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -12,14 +16,37 @@ public class Driver1 {
     private static WebDriver driver;
 
     public static WebDriver getDriver(){
-        if (driver==null) {  // if driver in not null, then create a new driver; otherwise just return the used driver
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+        if (driver==null) { //if driver is not in use(driver==null), there create a new driver. otherwise just return the  same driver
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().addArguments("--headless=new"));
+                    break;
+//                case "opera":
+//                    WebDriverManager.operadriver().setup();
+//                    driver = new OperaDriver();
+//                    break;
+            }
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
             driver.manage().window().maximize();
-        }
-        return driver;
+        }       return driver;
     }
 
     public static void closeDriver(){
